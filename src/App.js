e2374301a7c815e3c0ba587317b8817f64b0a898
Andres Castilla <andrescas1948@gmail.com>
2021-03-01 12:18:46 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import marked from 'marked';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const initialSate =`
+# heading 1
+## heading 2
+### heading 3
+
+- list item 1
+- list item 2
+- list item 3
+
+[visit my git hub](https://https://github.com/Cas147)
+
+this is a inline \`<div><div>\`
+
+**Bolded text**
+
+this is a quote= 
+\`\`\`
+> Block Quotes!
+\`\`\`
+
+![React Logo w/ Text](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqHQhQ0ZrsjuACsQizD7gpXPFQe5vboAmZWg&usqp=CAU)
+`
+class  App extends React.Component {
+  state ={
+    text:initialSate,
+  }
+
+  handleChange =(e)=>{
+    this.setState({
+      text:e.target.value
+    })
+  }
+  render(){
+    const {text}=this.state;
+    const markdown = marked(text)
+    return (
+      <div>
+        <h1 className="text-center m-4" >Convert your Markdown</h1>
+        <div className="row">
+            <div className="col-6">
+              <h4 className="text-center">Enter you markdown here</h4>
+              <textarea className="editor form-control p-2" value={text} onChange={this.handleChange}/>
+            </div>
+            <div className="col-6">
+            <h4 className="text-center">See the result</h4>
+            <div className="preview rounded" dangerouslySetInnerHTML={{__html: markdown}}>
+            </div>
+              </div>
+        </div>
+      </div>
+    );
+}
 }
 
 export default App;
